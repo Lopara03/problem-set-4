ee409f385227308ab81b6a9822e40912978a1d5e
Lopara03 <lopara@terpmail.umd.edu>
2025-08-17 18:12:57 -0400

    my code

--- src/part1_etl.py
+++ src/part1_etl.py
@@ -11,8 +11,27 @@ import os
 import pandas as pd
 import json
 
-# Create '/data' directory if it doesn't exist
-data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
-os.makedirs(data_dir, exist_ok=True)
+def etl():
 
-# Load datasets and save to '/data'
+# Create '/data' directory if it doesn't exist
+    data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
+    os.makedirs(data_dir, exist_ok=True)
+
+    # Load datasets and save to '/data'
+    url = "https://github.com/cbuntain/umd.inst414/blob/main/data/imdb_movies_2000to2022.prolific.json?raw=true"
+    df = pd.read_json(url, lines=True)
+
+    #cleaning: drop duplicates, reset index
+    df = df.drop_duplicates().reset_index(drop=True)
+
+# seeing if unique ID column exists
+    if "movie_id" not in df.columns:
+            df["movie_id"] = df.index + 1
+
+# saving cleaned data for later analysis
+    cleaned_path = os.path.join(data_dir, "imdb_movies_cleaned.csv")
+    df.to_csv(cleaned_path, index=False)
+
+    print(f" ETL complete. Cleaned dataset saved to {cleaned_path}")
+
+    return df
